fa7d172444dc861294fde0e350e6c9f1afd1f6b2
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-12-12 09:21:36 +0900

    Phongモデルの実装準備

--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -2,12 +2,21 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
+	
 	//右、下、奥の方向を向いたライト
 	float3 lightdir = float3(1.0f, -1.0f, 1.0f);
 	lightdir = normalize(lightdir);
 	//ライトの色
 	float3 lightcolor = float3(1.0f, 1.0f, 1.0f);
 
+	//環境反射光
+	float3 ambient = m_ambient;
+	//拡散反射光
+	float3 diffuse = dot(-lightdir, normal) * m_diffuse;
+	
+	//視点座標
+	const float3 eye = float3(0.0f, 0.0f, -20.0f);
+	
 	VSOutput output; // ピクセルシェーダーに渡す値
 	output.svpos = mul(mat, pos);
 	
