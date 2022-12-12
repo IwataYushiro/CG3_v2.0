708b8578ef494bcddba0f5748dd507e3ec6ee0da
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-12-12 09:27:48 +0900

    頂点シェーダ変更完了

--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -16,12 +16,20 @@ VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOOR
 	
 	//視点座標
 	const float3 eye = float3(0.0f, 0.0f, -20.0f);
+	//光沢度
+	const float shininess = 4.0f;
+	//頂点から視点への方向ベクトル
+	float3 eyedir = normalize(eye - pos.xyz);
+	//反射光ベクトル
+	float3 reflect = normalize(lightdir + 2 * dot(-lightdir, normal) * normal);
+	//鏡面反射光
+	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
 	
 	VSOutput output; // ピクセルシェーダーに渡す値
 	output.svpos = mul(mat, pos);
 	
 	//ランバート反射の計算
-	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.rgb = (ambient + diffuse + specular) * lightcolor;
 	output.color.a = m_alpha;
 	output.uv = uv;
 	return output;
