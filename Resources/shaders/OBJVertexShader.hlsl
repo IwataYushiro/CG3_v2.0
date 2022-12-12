e9d98449436e0001735f8d4cec3e69d615eb12af
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-12-12 09:14:41 +0900

    Lambertモデルを実装

--- Resources/shaders/OBJVertexShader.hlsl
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -2,9 +2,18 @@
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
+	//右、下、奥の方向を向いたライト
+	float3 lightdir = float3(1.0f, -1.0f, 1.0f);
+	lightdir = normalize(lightdir);
+	//ライトの色
+	float3 lightcolor = float3(1.0f, 1.0f, 1.0f);
+
 	VSOutput output; // ピクセルシェーダーに渡す値
 	output.svpos = mul(mat, pos);
-	output.normal = normal;
+	
+	//ランバート反射の計算
+	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.a = m_alpha;
 	output.uv = uv;
 	return output;
 }
